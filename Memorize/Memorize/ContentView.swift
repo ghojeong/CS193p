14a69522cd5ff971e772a66f1abf3cdf3ad376f0
고정완 (Can) <jw.gho@ecubelabs.com>
2020-07-19 16:27:01 +0900

    Initialize lecture1 Memorize

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by 고정완 on 2020/07/19.
+//  Copyright © 2020 Can_E3. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
